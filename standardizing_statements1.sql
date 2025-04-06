08b8b19972065ec4bc958dd9b93f1e58aa83bf89
Isaiah633 <isaiahvorotinov@gmail.com>
2025-04-05 22:37:58 -0400

    Update standardizing_statements1.sql

--- standardizing_statements1.sql
+++ standardizing_statements1.sql
@@ -6,8 +6,15 @@ WITH chase_merge AS
 -- Credit (positive) END AS normalized_amount, 'CHASE' AS Source FROM big-query1-447223.amex.chase ), 
 amex_merge AS ( SELECT Date, Description, amount, CASE WHEN amount > 0 THEN -amount -- Debit (positive, convert to negative)
  ELSE -amount -- Credit (negative, convert to positive) END AS normalized_amount, 'AMEX' AS Source FROM big-query1.amex.amex ), 
- discover_merge AS ( SELECT Date, Description, amount, CASE WHEN amount > 0 THEN -amount -- Debit (positive, convert to negative
- ELSE -amount -- Credit (negative, convert to positive) END AS normalized_amount, 'DISCOVER' AS Source FROM big-query1.amex.discover ), delta_merge AS ( SELECT Date, Description, amount, CASE WHEN amount > 0 THEN -amount -- Debit (positive, convert to negative) ELSE -amount -- Credit (negative, convert to positive) END AS normalized_amount, 'DELTA' AS Source FROM big-query1.amex.delta ), WF_merge AS ( SELECT Date, Description, amount, CASE WHEN amount < 0 THEN amount -- Debit (negative) ELSE amount -- Credit (positive) END AS normalized_amount, 'WFchecking' AS Source FROM big-query1.amex.WFchecking ), checking_merge AS ( SELECT Date, Description, amount, CASE WHEN Transaction = 'Debit' THEN -amount -- Debit (positive, convert to negative) WHEN Transaction = 'Credit' THEN amount -- Credit (leave as positive) ELSE NULL -- Optional: Handle unexpected Transaction values END AS normalized_amount, 'CHECKING' AS Source FROM big-query1.amex.checking )
+discover_merge AS ( SELECT Date, Description, amount, CASE WHEN amount > 0 THEN -amount -- Debit (positive, convert to negative
+ ELSE -amount -- Credit (negative, convert to positive) END AS normalized_amount, 'DISCOVER' AS Source FROM big-query1.amex.discover ),
+delta_merge AS ( SELECT Date, Description, amount, CASE WHEN amount > 0 THEN -amount 
+ -- Debit (positive, convert to negative) ELSE -amount -- Credit (negative, convert to positive) END AS normalized_amount, 
+ 'DELTA' AS Source FROM big-query1.amex.delta ), WF_merge AS ( SELECT Date, Description, amount, CASE WHEN amount < 0 THEN amount -- Debit (negative) ELSE amount 
+ -- Credit (positive) END AS normalized_amount, 'WFchecking' AS Source FROM big-query1.amex.WFchecking ), 
+checking_merge AS ( SELECT Date, Description, amount, CASE WHEN Transaction = 'Debit' THEN -amount
+ -- Debit (positive, convert to negative) WHEN Transaction = 'Credit' THEN amount -- Credit (leave as positive) 
+ END AS normalized_amount, 'CHECKING' AS Source FROM big-query1.amex.checking )
 
 -- Combine all transactions 
 SELECT * FROM chase_merge UNION ALL SELECT * FROM amex_merge UNION ALL SELECT * FROM discover_merge UNION ALL SELECT * FROM 
